cf1f875f23fd56fdb62d9a229b250f1bcfce1456
Yasmin <yasminbanu5112@gmail.com>
2024-08-25 00:41:27 +0530

    Update job functionality API

--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -3,6 +3,7 @@ import {
     getAllJobs,
     postJob,
     getMyJobs,
+    updateJob,
   } from "../controllers/jobController.js";
   import { isAuthenticated } from "../middlewares/auth.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/getall", getAllJobs);
 router.post("/post", isAuthenticated, postJob);
 router.get("/getmyjobs", isAuthenticated, getMyJobs);
+router.put("/update/:id", isAuthenticated, updateJob);
 
 export default router;
