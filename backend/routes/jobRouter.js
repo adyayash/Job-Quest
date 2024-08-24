35d09f69adca7c8d430b87f47d76d45b6a166f83
Yasmin <yasminbanu5112@gmail.com>
2024-08-24 23:43:42 +0530

    Get and Post job functionality API

--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import {
+    getAllJobs,
+    postJob,
+  } from "../controllers/jobController.js";
+  import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
+
+router.get("/getall", getAllJobs);
+router.post("/post", isAuthenticated, postJob);
 
 export default router;
